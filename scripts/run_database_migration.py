a1ed8d11f0ca330f535d3b90ce44964fa6a45657
scorsi <sylvain.corsini@gmail.com>
2018-06-07 22:49:13 +0200

    Update the migrations scripts

--- scripts/run_database_migration.py
+++ scripts/run_database_migration.py
@@ -1,24 +1,69 @@
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
+import psycopg2
 import os
 
 
-def main():
-    auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-    cluster = Cluster(auth_provider=auth_provider)
-    session = cluster.connect()
+def migrate_models():
+    conn = psycopg2.connect(
+        dbname="viny",
+        user="admin",
+        password="password123",
+        host="localhost",
+        port=5432
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
 
-    for model in os.listdir('../models/'):
-        if os.path.isfile('../models/{}'.format(model)):
-            with open('../models/{}'.format(model), mode='r') as f:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS migrations (
+            version SMALLINT NOT NULL
+        );
+        SELECT version FROM migrations LIMIT 1;
+    """)
+
+    actual_version = cur.fetchone()
+    if actual_version is None:
+        print('First deployment, starting from migration 000')
+        actual_version = 0
+    else:
+        print('Starting deployment from migration {}'.format(actual_version[0]))
+    actual_version = int(actual_version[0])
+    version = 0
+
+    models = os.listdir('../models/')
+    models.sort()
+    for model in models:
+        filename = '../models/{}'.format(model)
+        version = int(filename.split('/')[2].split('_')[0])
+        if version <= actual_version:
+            continue
+        print('Executing statements in file {}'.format(filename))
+        if os.path.isfile(filename):
+            with open(filename, mode='r') as f:
                 txt = f.read()
                 stmts = txt.split(';')
                 for i in stmts:
                     stmt = i.strip()
                     if stmt != '':
                         print('Executing "' + stmt + '"')
-                        session.execute(stmt)
+                        cur.execute(stmt)
+
+    cur.execute("""
+        DELETE FROM migrations;
+        INSERT INTO migrations (version) VALUES ('{}');
+    """.format(version))
+
+    cur.execute("""
+        SELECT table_schema, table_name, table_type
+            FROM information_schema.tables
+            WHERE table_schema <> 'pg_catalog' AND
+                table_schema <> 'information_schema'
+            ORDER BY table_schema ASC, table_name ASC;
+    """)
+    tables = cur.fetchall()
+    print('Tables in database:')
+    for table in tables:
+        print('\t{}: {}.{}'.format(table[2], table[0], table[1]))
 
 
 if __name__ == '__main__':
-    main()
+    migrate_models()
